e299138e3b650f87272eb538ffe50c4ae1e30b37
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-12-01 23:05:39 +0530

    First login screen using 1 Image 2 textfields and 1 button created.

---
+++ LoginScreen AutoLoyout/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  LoginScreen AutoLoyout
+//
+//  Created by Prashant G on 11/30/18.
+//  Copyright © 2018 MyOrg. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let tropicBlue = UIColor(red: 0, green: 192/255, blue: 255/255, alpha: 1)
+}
+
+
+struct Fonts {
+    static let avenirNextCondensedDemiBold = "AvenirNextCondensed-DemiBold"
+}
--- LoginScreen AutoLoyout/ViewController.swift
+++ LoginScreen AutoLoyout/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    var backgroundImage = UIImageView()
+    @IBOutlet var logoImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        logoImageView.layer.cornerRadius = logoImageView.frame.width/2
+        logoImageView.layer.masksToBounds = true
+        setupBackgroundImage()
     }
 
 
+    func setupBackgroundImage() {
+        view.addSubview(backgroundImage)
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        
+        backgroundImage.image = UIImage(named: "ocean")
+        view.sendSubviewToBack(backgroundImage)
+    }
 }
 
